c06c207a9257bf487950f18b3d90c3f9fc93945f
Nhan Phan <57086421+nhanphan0411@users.noreply.github.com>
2025-11-01 13:30:27 +0700

    fix: case sensitive path for video pages

--- src/pages/artworkpages/TaisKoshinoPage.jsx
+++ src/pages/artworkpages/TaisKoshinoPage.jsx
@@ -1,4 +1,4 @@
-import VideoPages from "../videopages.jsx";import "../videopages.css";
+import VideoPages from "../VideoPages.jsx";import "../videopages.css";
 
 export default function TaisKoshinoPage() {
   return (
